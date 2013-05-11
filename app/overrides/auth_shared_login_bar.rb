e198014d80667faa202486df28a8fe40b3c96f17
psymeon <petros.symeonidis@gmail.com>
2013-05-11 18:36:43 +0300

    Update auth_shared_login_bar.rb

--- app/overrides/auth_shared_login_bar.rb
+++ app/overrides/auth_shared_login_bar.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
+Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :name => "auth_shared_login_bar",
-                     :insert_before => "li#search-bar",
+                     :insert_bottom => "ul#main-nav-bar",
                      :partial => "spree/shared/login_bar",
                      :disabled => false, 
                      :original => 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac')
